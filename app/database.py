21f3c318dc9dece3992d49b1bb29f29eb998ade9
Roman Sivuda <Romanytsch@gmail.com>
2026-03-12 16:38:10 +0300

    create endpoints

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ DATABASE_URL = (f"postgresql+asyncpg://"
                 f"{os.getenv('POSTGRES_PORT')}/"
                 f"{os.getenv('POSTGRES_DB')}")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, pool_pre=True)
 AsyncSessionLocal = async_sessionmaker(engine,
                                        class_=AsyncSession,
                                        expire_on_commit=False)
@@ -21,5 +21,9 @@ async def get_db() -> AsyncSession:
     async with AsyncSessionLocal() as session:
         try:
             yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
         finally:
             await session.close()
